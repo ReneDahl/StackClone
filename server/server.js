5383e37a0c5bb9cd63e8351762867aaff15050ec
RD1988 <rene_dahl@hotmail.com>
2019-11-18 00:24:32 +0100

    changing routes....

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static("../client/build"));
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
