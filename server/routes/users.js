de5cb018160dfac5c0b4269b0769f79a01ed4b8d
RD1988 <rene_dahl@hotmail.com>
2019-12-15 22:40:47 +0100

    checks

--- server/routes/users.js
+++ server/routes/users.js
@@ -15,15 +15,14 @@ server.route("/create").post((req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  //if username or password is empty, it will display the msg
-  if (!username) {
+  const user = { username: username, password: password };
+
+  if (!user) {
     let msg = "Username or password missing!";
     console.error(msg);
     res.status(401).json({ msg: msg });
     return null; //Inportent if it not return null here, it will keep running.
   }
-
-  const user = { username: username };
 
   user
     .save()
